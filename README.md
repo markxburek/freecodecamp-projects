066e97d0feaa14a8da3ac21aadd025d2fc2db946
markxburek <markxburek@gmail.com>
2019-11-30 16:25:57 -0600

    Completed Palindrome Checker project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # freecodecamp-projects
-Projects I did to get certified on FreeCodeCamp.org
+These are projects I did to get certified on FreeCodeCamp.org. 
+
+Each folder represents a group of projects necessary for the certification.
+
+You can find out more information here: https://www.freecodecamp.org/learn
+
+
---
+++ javascript-algorithms-and-data-structures/palindromeChecker.js
@@ -0,0 +1,30 @@
+/*
+Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+Note
+You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+
+We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
+
+We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+
+*/
+
+function palindrome(str) {
+
+    // w includes the '_', so needed to use longhand method
+    let onlyAlphaNumericLowerCaseArray = str.toLowerCase().match(/[A-Za-z0-9]/g);
+
+    let reversedAlphaNumericLowerCaseArray = [...onlyAlphaNumericLowerCaseArray];
+    reversedAlphaNumericLowerCaseArray.reverse();
+     
+
+    for (let i = 0; i < onlyAlphaNumericLowerCaseArray.length; i++) {
+        if (onlyAlphaNumericLowerCaseArray[i] !== reversedAlphaNumericLowerCaseArray[i])
+            return false;
+    }
+    return true;
+}
+
